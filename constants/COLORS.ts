d3e16a0acc1ee6bb5e6aab3986f6591b6bdf42a9
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2025-01-24 13:31:42 +0700

    feature: added file color project

---
+++ constants/COLORS.ts
@@ -0,0 +1,4 @@
+export const COLOR_PRIMARY = '#A59D84';
+export const COLOR_SECONDARY = '#C1BAA1';
+export const COLOR_TERTIARY = '#D7D3BF';
+export const COLOR_QUATERNARY = '#ECEBDE';
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,29 @@
 import type { Config } from "tailwindcss";
+import {
+    COLOR_PRIMARY,
+    COLOR_QUATERNARY,
+    COLOR_SECONDARY,
+    COLOR_TERTIARY,
+} from "./constants/COLORS";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                COLOR_PRIMARY: COLOR_PRIMARY,
+                COLOR_SECONDARY: COLOR_SECONDARY,
+                COLOR_TERTIARY: COLOR_TERTIARY,
+                COLOR_QUATERNARY: COLOR_QUATERNARY,
+            },
+            boxShadow: {
+                intense: "2px 5px 10px 2px rgba(0,0,0,0.2)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
