4ddb64feba87d9f0905285c49e1ac3cd3bdce547
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2025-01-24 13:17:09 +0700

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+export default function Navbar({}: Props) {
+    return <div className="flex justify-between">
+        <div className=""><span className="">Pawaris</span><span>Rabeab</span></div>
+        <div></div>
+        <div></div>
+    </div>;
+}
