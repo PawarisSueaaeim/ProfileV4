9be4c5f530aa84eddab43a1723b94523527f769c
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2025-02-03 18:18:22 +0700

    typing

--- common/uicomp/Typing.tsx
+++ common/uicomp/Typing.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { RootState } from "@/lib/store";
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 type Props = {
     text: string;
@@ -11,22 +13,34 @@ export default function Typing({ text, delay, className }: Props) {
     const [currentText, setCurrentText] = useState<string>("");
     const [currentIndex, setCurrentIndex] = useState<number>(0);
 
+    const { language } = useSelector(
+        (state: RootState) => state.onChangeMultiLanguageSlice
+    );
+
+    const handleResetCurrentText = () => {
+        setCurrentText("");
+        setCurrentIndex(0);
+    };
+
+    useEffect(() => {
+        handleResetCurrentText();
+    }, [language, text]);
+
     useEffect(() => {
         if (currentIndex < text.length) {
             const timeout = setTimeout(() => {
                 setCurrentText((prevText) => prevText + text[currentIndex]);
                 setCurrentIndex((prevIndex) => prevIndex + 1);
-            }, delay/currentIndex);
+            }, delay / currentIndex);
 
             return () => clearTimeout(timeout);
         }
         if (currentText === text) {
             setTimeout(() => {
-                setCurrentText("");
-                setCurrentIndex(0);
-            },2000);
+                handleResetCurrentText();
+            }, 2000);
         }
-    }, [currentIndex, delay, text]);
+    }, [currentIndex, delay, text, language]);
 
     return <div className={className}>{currentText}</div>;
 }
