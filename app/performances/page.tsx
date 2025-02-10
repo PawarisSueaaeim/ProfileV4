ed2f5c8b474010e00a4d08104392f20511cd4980
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2025-02-10 08:34:10 +0700

    feature: performance page

--- app/performances/page.tsx
+++ app/performances/page.tsx
@@ -4,15 +4,22 @@ import React from "react";
 type Props = {};
 
 export default function Performance({}: Props) {
-    const rapidAutoImages = [
+    const rapidImages = [
         { id: "001", image: "/pictures/rapid-dashboard-01.png" },
         { id: "002", image: "/pictures/rapid-dashboard-02.png" },
         { id: "003", image: "/pictures/rapid-dashboard-03.png" },
-        { id: "004", image: "/pictures/rapid-dashboard-04.png" },
+        { id: "004", image: "/pictures/rapid-auto-01.png" },
+        { id: "005", image: "/pictures/rapid-auto-02.png" },
+        { id: "006", image: "/pictures/rapid-auto-03.png" },
+        { id: "007", image: "/pictures/rapid-life-01.png" },
+        { id: "008", image: "/pictures/rapid-life-02.png" },
+        { id: "009", image: "/pictures/rapid-life-03.png" },
+        { id: "010", image: "/pictures/rapid-now-01.png" },
+        { id: "011", image: "/pictures/rapid-now-02.png" },
     ];
     return (
-        <div className="min-h-screen flex justify-center">
-            <Carousel imageDatas={rapidAutoImages} />
+        <div className="min-h-screen">
+            <Carousel imageDatas={rapidImages} />
         </div>
     );
 }
--- components/carousel/Carousel.tsx
+++ components/carousel/Carousel.tsx
@@ -11,18 +11,24 @@ type Props = {
 };
 
 export default function Carousel({ imageDatas }: Props) {
-    return <div className="flex justify-center w-screen">
-        {imageDatas.map((item, index) => {
-            return (
-                <div key={`${item.id}-${index}`} className="absolute">
-                    <Image
-                        src={item.image}
-                        alt={item.image}
-                        height={500}
-                        width={500}
-                    />
-                </div>
-            );
-        })}
-    </div>;
+    return <div className="grid grid-cols-1 md:grid-cols-2">
+        <div className="flex justify-center items-center w-screen">
+            {imageDatas.map((item, index) => {
+                return (
+                    <div key={`${item.id}-${index}`} className="">
+                        <Image
+                            src={item.image}
+                            alt={item.image}
+                            height={500}
+                            width={500}
+                            className="object-cover w-full"
+                        />
+                    </div>
+                );
+            })}
+        </div>;
+        <div className="flex justify-center items-center">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo facilis et dolor sequi, illum consequuntur maxime error amet aliquam voluptates repudiandae perspiciatis! Possimus vel explicabo saepe id harum dolores natus?
+        </div>
+    </div>
 }
