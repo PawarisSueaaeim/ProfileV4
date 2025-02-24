f4cc74c3b0baee3517e03aaf8139ac2fc1fc83f7
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2025-02-24 15:39:13 +0700

    feature: test jest

--- components/skills/CardSkills.test.tsx
+++ components/skills/CardSkills.test.tsx
@@ -1,10 +1,13 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import CardSkills from "./CardSkills";
+import { sum } from "@/utils/function/sum";
 
 describe("CardSkills Component", () => {
     it("renders the CardPrimary with a title", () => {
         render(<CardSkills/>)
+
+        expect(sum(2,3)).toBe(5);
     });
 
     // it("renders all skill items", async () => {
